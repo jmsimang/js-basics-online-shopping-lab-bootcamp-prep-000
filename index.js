4808ce10fe2fdef95295d16c0daf90311c8471cb
Jabulani Msimango <msimango.jabulani@moov.life>
2018-04-12 10:43:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,44 +10,44 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart[item] = Math.floor(Math.random() * 100) + 1
+  cart.push({[item]: Math.floor(Math.random() * 100) + 1})
   console.log(`${item} has been added to your cart.`)
   return cart
 }
 
 function viewCart() {
+  if (cart.length == 0)
+    return "Your shopping cart is empty."
   var message = "In your cart, you have "
   for (var key in cart) {
     message += `${key} at $${cart[key]} and `
   }
-  /*
-  for (let i = 0; i < cart.length; i++) {
-    if (cart.length == 0)
-      return "Your shopping cart is empty."
-    if (i < cart.length - 1)
-      message += `${cart[i]} at $${cart[i]} and `
-    else
-      message += `${cart[i]} at $${cart[i]}.`
-  }*/
   return message
 }
 
 function total() {
   var total = 0
-  for (var key in cart) {
-    total += cart[key]
+  for (let i = 0; i < cart.length; i++) {
+    total += cart.i[i]
   }
-  /*
-  for (i = 0; i < cart.length; i++) {
-    total += cart[i]
-  }*/
   return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if (cart.hasOwnProperty(item)) {
+    delete cart[item]
+    return cart
+  } else
+    return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  
+  if (cardNumber === "")
+    return "Sorry, we don't have a credit card on file for you."
+  else {
+    var amount = total();
+    cart.length = 0
+    return `Your total cost is $${amount}, which will be charged to the card ${cardNumber}.`
+  }
 }
